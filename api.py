e22fc76dea32ff9a9e4cde2dc15ffe0d5db50b35
ArnaldoAlicea-Rosario <arnaldohalicea@gmail.com>
2025-02-14 10:37:16 -0400

    fixed issues, needs multithreading wget and make everything into one file for easy management

--- api.py
+++ api.py
@@ -8,11 +8,14 @@ def get(url):
     else: return None
 
 def wget(url, fname, dir):
-    os.system(f"wget {url} -O {fname} -P {dir}")
+    os.system(f"wget {url} -O {dir}/{fname}.cbz -q --show-progress")
     
 def makefolder(name):
     os.system(f"mkdir comics/{name}")
 
 def download(l, info):
-    print(f"Downloading issue {l[1]}...")
     wget(l[0], f'{info[1]}-{l[1]}', f"comics/{info[1]}")
+    
+def downthread(links, info):
+    while len(links) > 0:
+        download(links.pop(), info)
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from api import *
 import search
-from colorama import Fore, init
+from colorama import Fore
+from tqdm import tqdm
 from threading import Thread
-init()
 
 maxthreads = 4
 
 def downloadcomics(u=None, n=None):
     if u is None:
-        print(Fore.GREEN + "Enter lastest URL for comic")
+        print(Fore.GREEN + "Enter lastest URL for comic" + Fore.RESET)
         u = input('url: ')
     if n is None:
-        print(Fore.CYAN + "Enter the number of last issue downloaded (skippable)")
+        print(Fore.CYAN + "Enter the number of last issue downloaded (skippable)" + Fore.RESET)
         n = input('iss: ')
         if n == '':
             n = 0
@@ -24,7 +24,8 @@ def downloadcomics(u=None, n=None):
     end = n
     year = info[3]
     dlinks = []
-    for i in range(sta,end,-1):
+    print(Fore.YELLOW + "Getting download links..." + Fore.RESET)
+    for i in tqdm(range(sta,end,-1)):
         while True:
             r = get(base + f"/{info[1]}-{i}-{year}/")
             if r != None:
@@ -32,9 +33,9 @@ def downloadcomics(u=None, n=None):
             year -= 1
         urls = search.url(r)
         dlinks.append((urls[0][0],i))
-    for l in dlinks:
-        download(l, info) # add multi-threading here
-    print(Fore.GREEN + "Download complete")
+    print(Fore.YELLOW + "Downloading..." + Fore.RESET)
+    downthread(dlinks, info)
+    print(Fore.GREEN + "Download complete" + Fore.RESET)
         
 if __name__ == "__main__":
     downloadcomics()
